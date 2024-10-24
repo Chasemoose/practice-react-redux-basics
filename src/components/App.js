966c619bdb396dd236c0e83144861a44b87d8ba1
Jaro <jaroswidnik@interia.pl>
2024-10-24 16:14:55 +0200

    Finished ex 3

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,28 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-const store = createStore(() => {
-    return {
-        message: 'Działa!'
+const initState = {
+        message: 'Działa!',
+        time: new Date()
+}
+
+const reducer = (state = initState, action) => {
+    switch(action.type) {
+        case 'getCurrentTime':
+            return {...state, time: new Date()}
     }
-})
+    return state
+}
+
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
 
 const App = () => {
     return (
         <Provider store={store}>
             <Task01 />
             <Task02 />
-            {/* <Task03 /> */}
+            <Task03 />
             {/* <Task04 /> */}
             {/* <Task05 /> */}
         </Provider>
